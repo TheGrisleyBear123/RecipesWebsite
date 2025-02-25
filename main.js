58795836c03831d7d0ee74743d6fbdff7b6ca993
rishsheth <91674813+TheGrisleyBear123@users.noreply.github.com>
2025-02-25 17:49:36 +0000

    Day 2 Slider material COMPLETE AT HOME!!!!!!!

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-
+    //prepTime, cookTime, additionalTime, totalTime, numPeople, numBurritos
+    const prepTime = document.getElementById("prep_time"); 
+    const cookTime = document.getElementById("cook_time"); 
+    const additionalTime = document.getElementById("additional_time"); 
+    const totalTime = document.getElementById("total_time"); 
+    const numPeople = document.getElementById("num_people"); 
+    const numBurritos = document.getElementById("num_burritos");
+    //Slider 
+    const slider = document.getElementById("myRange"); 
+    slider.addEventListener("input", () => {
+        let sliderVal = slider.value; 
+        numBurritos.innerText = sliderVal + " Burritos"; 
+        numPeople.innerText = sliderVal/2 + " People"; 
+    });
 
 }); 
