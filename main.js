a3ccc71763d05b076272ad062933014fa8648530
rishsheth <91674813+TheGrisleyBear123@users.noreply.github.com>
2025-02-25 20:06:24 +0000

    Added slider code

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //prepTime, cookTime, additionalTime, totalTime, numPeople, numBurritos
     const prepTime = document.getElementById("prep_time"); 
     const cookTime = document.getElementById("cook_time"); 
     const additionalTime = document.getElementById("additional_time"); 
     const totalTime = document.getElementById("total_time"); 
     const numPeople = document.getElementById("num_people"); 
     const numBurritos = document.getElementById("num_burritos");
-    //Slider 
-    const slider = document.getElementById("myRange"); 
+
+    const slider = document.getElementById("myRange");
+
     slider.addEventListener("input", () => {
-        let sliderVal = slider.value; 
+        let sliderVal = parseInt(slider.value); 
         numBurritos.innerText = sliderVal + " Burritos"; 
-        numPeople.innerText = sliderVal/2 + " People"; 
+        numPeople.innerText = (sliderVal / 2).toFixed(1) + " People"; 
+        prepTime.innerText = (10 + sliderVal) + " minutes"; 
+        cookTime.innerText = Math.floor(60 + sliderVal * 2) + " minutes"; 
+        additionalTime.innerText = (20 + (sliderVal * 0.5)).toFixed(1) + " minutes"; 
+        let total = parseFloat(prepTime.innerText) + parseFloat(cookTime.innerText) + parseFloat(additionalTime.innerText);
+        totalTime.innerText = total + " minutes"; 
     });
 
-}); 
+});
